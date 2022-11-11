bf562f52140648c5780d7403f1aee1440e405952
Nikita <nikitka_bodr@mail.ru>
2022-11-11 23:34:52 +0300

    update validation authors book

--- app/models/authors_book.rb
+++ app/models/authors_book.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AuthorsBook < ApplicationRecord
   belongs_to :book
   belongs_to :author
-  validates :book, presence: true
-  validates :genre,presence: true
-  validates :book, uniqueness: {scope: :author}
+  validates :book_id, presence: true
+  validates :author_id, presence: true
+  validates :book_id, uniqueness: { scope: :author_id }
 end
