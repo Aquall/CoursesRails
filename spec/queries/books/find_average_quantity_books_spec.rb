2e9b010e96bec77b922942474526c6c9891211ef
Nikita <nikitka_bodr@mail.ru>
2022-11-12 17:11:56 +0300

    fix

--- spec/queries/books/find_average_quantity_books_spec.rb
+++ spec/queries/books/find_average_quantity_books_spec.rb
@@ -4,9 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Books::FindAverageQuantityBooks do
   before do
-    #@subscriber = create(:subscriber)
-    #create_list(:subscription, 10, subscriber: @subscriber)
-    #create_list(:subscriber_with_subscriptions, 10, subscriptions_count: 10)
     create_list(:book, 10, quantity: 100)
     create_list(:book, 10, quantity: 200)
   end
