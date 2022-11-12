060badb46c376bf04f22e3ecfe0ff4969c618b6c
Nikita <nikitka_bodr@mail.ru>
2022-11-12 17:09:29 +0300

    rubocop fix 2.0

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,19 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  describe 'associations' do
+    subject { create(:author) }
 
-    
+    it { is_expected.to have_many(:authors_books) }
+    it { is_expected.to have_many(:books).through(:authors_books) }
+  end
 
-    describe 'associations' do
-        subject { create(:author) }
+  describe 'validations' do
+    subject { build(:author) }
 
-        it { should have_many(:authors_books) }
-        it { should have_many(:books).through(:authors_books)}
-    end
-    
-    describe 'validations' do
-        subject {build(:author)}
-
-        it { should validate_presence_of(:name).with_message('must be abided')}
-    end
+    it { is_expected.to validate_presence_of(:name).with_message('must be abided') }
+  end
 end
