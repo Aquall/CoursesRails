f6a83f44ec05e1a03fd9db838fe06a8ffea2e3f1
Nikita <nikitka_bodr@mail.ru>
2022-11-12 15:11:06 +0300

    add book spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,14 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-    context 'associations' do
-        it { should have_many(:books_genres) }
-        it { should have_many(:genres).through(:books_genres)}
-        it { should have_many(:authors_books)}
-        it { should have_many(:authors).through(:authors_books)}
-    end
-    
-    describe 'validations' do
-        it { should validate_uniqueness_of(:name).scoped_to(:year).with_message('must be unique!')}
-    end
+  describe 'associations' do
+    subject { create(:book) }
+
+    it { is_expected.to have_many(:books_genres) }
+    it { is_expected.to have_many(:genres).through(:books_genres) }
+    it { is_expected.to have_many(:authors_books) }
+    it { is_expected.to have_many(:authors).through(:authors_books) }
+  end
+
+  describe 'validations' do
+    subject { build(:book) }
+
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:year).with_message('must be unique!') }
+  end
 end
