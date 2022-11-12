8e77076e77ebb8eef73027f46fae71d10754bbd3
Nikita <nikitka_bodr@mail.ru>
2022-11-12 14:53:21 +0300

    add validation to model subscriber and subscriber spec

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,4 +2,6 @@ class Subscriber < ApplicationRecord
     has_many :subscriptions
     has_many :active_subscriptions,-> {where(is_active: true)}, class_name: "Subscription"
     has_many :documents, class_name: "Subscription"
+
+    validates :name, presence: {message: 'must be abided'}
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -4,11 +4,13 @@ require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
   describe 'associations' do
-    before do
-      create(:subscriber)
-    end
-
+    subject { create(:subscriber) }
     it { is_expected.to have_many(:subscriptions) }
   end
 
+  describe 'validations' do
+    subject {build(:subscriber)}
+
+    it { should validate_presence_of(:name).with_message('must be abided')}
+end
 end
