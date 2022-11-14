f306317d13e2ac2701d03455a81af69461835acb
Nikita <nikitka_bodr@mail.ru>
2022-11-14 15:12:41 +0300

    add status in factory book

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -5,6 +5,7 @@
 #  id         :integer          not null, primary key
 #  name       :string
 #  quantity   :integer          default(0)
+#  status     :integer          default(0)
 #  year       :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -18,8 +19,12 @@ FactoryBot.define do
         sequence(:name) { |n| "name#{n}" }
         year {rand(2017..2020)}
         quantity {rand(1..15)}
+        status {1}
         trait :old do
             year {rand(1900..1940)}
+        end
+        trait :archived do
+            status {0}
         end
         factory :books_with_genre do
             transient do
