0601957a37cb5a08453c5b5bf6edb4358d125bf1
Nikita <nikitka_bodr@mail.ru>
2022-11-14 15:06:46 +0300

    code ordering

--- app/queries/subscriptions/find_average_days_subscriptions_is_active_false.rb
+++ app/queries/subscriptions/find_average_days_subscriptions_is_active_false.rb
@@ -1,9 +1,12 @@
 module Subscriptions
     class FindAverageDaysSubscriptionsIsActiveFalse
         def self.call
-            Subscription.where(is_active: false)
+            Subscription.not_active
                         .group("julianday(finish) - julianday(start)")
-                        .count.keys.sum.to_i       
+                        .count
+                        .keys
+                        .sum
+                        .to_i       
         end
     end
 end
