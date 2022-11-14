eb7cbabc775fbec1aa8178c5748ae411bb2c51af
Nikita <nikitka_bodr@mail.ru>
2022-11-14 15:14:29 +0300

    add scope with_long_name

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@
 #  id         :integer          not null, primary key
 #  name       :string
 #  quantity   :integer          default(0)
+#  status     :integer          default(0)
 #  year       :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -18,5 +19,12 @@ class Book < ApplicationRecord
     has_many :genres, through: :books_genres
     has_many :authors_books
     has_many :authors, through: :authors_books
+
+    enum status: [ :active, :archived ]
+
+    accepts_nested_attributes_for :authors 
+    accepts_nested_attributes_for :genres
     validates :name, uniqueness:{scope: :year, message: 'must be unique!'}
+
+    scope :with_long_name, ->{ where("LENGTH(:name) > 10") }
 end
