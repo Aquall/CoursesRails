5738424561218da91283b59e8da1ed72ef3c9c43
Nikita <nikitka_bodr@mail.ru>
2022-11-17 19:40:23 +0300

    add employe

--- app/models/employe.rb
+++ app/models/employe.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: employes
+#
+#  id         :integer          not null, primary key
+#  integer    :string
+#  name       :string
+#  role       :string
+#  string     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Employe < ApplicationRecord
   
 end
--- db/migrate/20221116231112_create_employes.rb
+++ db/migrate/20221116231112_create_employes.rb
@@ -2,7 +2,7 @@ class CreateEmployes < ActiveRecord::Migration[7.0]
   def change
     create_table :employes do |t|
       t.string :name
-      t.string :string,
+      t.string :string
       t.string :role
       t.string :integer
 
