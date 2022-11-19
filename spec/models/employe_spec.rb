1a4b3597fcbc627b84c4ec3907290ea07b7159a2
Nikita <nikitka_bodr@mail.ru>
2022-11-19 03:39:17 +0300

    add model employe

--- spec/models/employe_spec.rb
+++ spec/models/employe_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: employes
+#
+#  id         :integer          not null, primary key
+#  integer    :string
+#  name       :string
+#  role       :string
+#  string     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Employe, type: :model do
