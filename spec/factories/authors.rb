b09c568138c1ca53746fa0b53bf40c631a387623
Nikita <nikitka_bodr@mail.ru>
2022-11-20 23:50:04 +0300

    add employes and fix factory author_with_books

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: authors
@@ -12,21 +14,21 @@
 #  index_authors_on_name  (name) UNIQUE
 #
 FactoryBot.define do
-    factory :author do
-        sequence(:name) { |n| "name#{n}" }
-        trait :in_valid do
-            name {'name1'}
-        end
-        factory :author_with_books do
-            transient do
-                books_count { 5 }
-            end
-            after(:create) do |authors, evaluator|
-                create_list(:book, evaluator.books_count, author: author)
-        
-                authors.reload
-            end
-        end
+  factory :author do
+    sequence(:name) { |n| "name#{n}" }
+    trait :in_valid do
+      name { 'name1' }
     end
+    factory :author_with_books do
+      transient do
+        books_count { 5 }
+      end
+      after(:create) do |authors, evaluator|
+        (0...evaluator.books_count).each do
+          authors.books << FactoryBot.create(:book)
+        end
+        authors.reload
+      end
+    end
+  end
 end
-  
--- spec/factories/employes.rb
+++ spec/factories/employes.rb
@@ -1,8 +1,12 @@
-FactoryBot.define do
-  factory :employe do
-    name { "MyString" }
-    string, { "MyString" }
-    role { "MyString" }
-    integer { "MyString" }
-  end
-end
+# == Schema Information
+#
+# Table name: employes
+#
+#  id         :integer          not null, primary key
+#  integer    :string
+#  name       :string
+#  role       :string
+#  string     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
