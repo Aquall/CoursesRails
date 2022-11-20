5a5eaaaaf767fde94905bc0382ad106b6e4cd617
Nikita <nikitka_bodr@mail.ru>
2022-11-20 22:50:25 +0300

    add error_handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
-  
+  rescue_from Exception, with: :error_handler
+  def error_handler(errors)
+    render json: errors, status: :unprocessable_entity
+  end
 end
