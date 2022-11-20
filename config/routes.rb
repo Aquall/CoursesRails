62502b03d3cac368dfd8d46bd4ac0551c4a0010c
Nikita <nikitka_bodr@mail.ru>
2022-11-20 06:02:39 +0300

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,23 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :authors
-      resources :genres do
-        resources :books
-      end
+      resources :genres
       resources :books
       resources :subscribers do
         resources :subscriptions
       end
+
+      namespace :admin do
+        resources :authors 
+        resources :genres
+        resources :books
+        resources :subscribers do
+          resources :subscriptions
+        end
+        resources :subscriptions
+      end
     end
   end
-
-  # namespace :api do
-  #   namespace :admin do
-  #     resources :authors do
-  #       resources :books do
-  #         resources :genres
-  #     end
-  #     resources :subscribers do
-  #       resources :subscriptions
-  #     end
-  #   end
-  # end
 
   resources :subscriptions
   resources :subscribers
