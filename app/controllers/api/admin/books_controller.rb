c8a17edaa270235b407e53f9ee40bcdd0f7453e5
Nikita <nikitka_bodr@mail.ru>
2022-11-22 08:04:58 +0300

    add service update for controller books and spec

--- app/controllers/api/admin/books_controller.rb
+++ app/controllers/api/admin/books_controller.rb
@@ -33,15 +33,8 @@ module Api
 
       # PATCH/PUT /books/1 or /books/1.json
       def update
-        respond_to do |format|
-          if @book.update(book_params)
-            format.html { redirect_to book_url(@book), notice: 'Book was successfully updated.' }
-            format.json { render :show, status: :ok, location: @book }
-          else
-            format.html { render :edit, status: :unprocessable_entity }
-            format.json { render json: @book.errors, status: :unprocessable_entity }
-          end
-        end
+        @book = ::Admin::Books::Update.call(params)
+        render json: BookBlueprint.render(@book), status: :accepted
       end
 
       # DELETE /books/1 or /books/1.json
